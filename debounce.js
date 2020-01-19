61798b56e44752acc627f05c774c053580f782c2
李起源 <liqiyuan@zuoshouyisheng.com>
2020-01-19 22:41:40 +0800

    debounce

---
+++ debounce.js
@@ -0,0 +1,30 @@
+module.exports = debounce
+function debounce(fn,delay,immediately){
+	let timer,args,context,result;
+
+	function debounced(){
+		context = this;
+		args = arguments;
+
+		timer && clearTimeout(timer);
+		if(immediately){
+			var callNow = !time;
+			timer = setTimeout(function(){
+				timer = null
+			},delay);
+			if(callNow){
+				result = fn.apply(context,args)
+			}
+		}else{
+			timer = setTimeout(function(){
+				result = fn.apply(context,args)
+			},delay)
+		}
+		return result;
+	}
+	debounced.clear = function(){
+		clearTimeout(timer)
+		timer = null
+	}
+	return debounced;
+}
